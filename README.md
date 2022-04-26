d47a6890664275c2d0028fb7d80a99ad5cc45f95
Simon Murray <simondonaldmurray@gmail.com>
2022-04-26 15:21:32 +0100

    updated organisation of cellbender script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cellbender
 Repo containing scripts for running cellbender
+
+Directory structure should be as followed:
+
+top level directories - actions, data, work
+
+actions - contains all the scripts which are used to run souporcell
+
+data - contains subdirectories for each sample with sampleid as name, output is written to these subdirectories
+
+work - contains logs subdirectory which stores all the log files for running cellbender
+
--- scripts/cellbender-0-2-0.sh
+++ scripts/cellbender-0-2-0.sh
@@ -2,39 +2,33 @@
 
 set -euo pipefail
 
-TAG=$1 #sampleID which will be used as directory name
-WDIR=`pwd` #setting current working path to a variable
-EPOCHS=150 #epochs value
-EXPECTED_CELLS=5000 #expected cells value
-DROPLETS=150000 #total droplets value
-FPR=0.01 #fpr value
-LEARN=0.0001 #learning value
+im=/nfs/cellgeni/singularity/images/cellbender0.2.0-pytorch1.11-cuda11.3.1-commit2507742.sif
 
-# add singularity to the path
-PATH="/software/singularity-v3.6.4/bin:$PATH"
+dir=$1 #sampleID which will be used as directory name
+epochs=150 #epochs value
+cells=5000 #expected cells value
+droplets=150000 #total droplets value
+fpr=0.01 #fpr value
+learn=0.0001 #learning value
 
-# set the path to the image we want to use
-CELLBENDER_IMAGE=/nfs/cellgeni/singularity/images/cellbender0.2.0-pytorch1.11-cuda11.3.1-commit2507742.sif
+cd ../data/$dir
 
-# path to output folder (samples will have a folder inside this one)
-OUTPUT_FOLDER="$WDIR/results/$TAG/outs"
-
-# create output folder if it does not exist
-if [[ ! -d "${OUTPUT_FOLDER}" ]]; then
-    mkdir -p "${OUTPUT_FOLDER}"
-fi
-
-# setting input path
-INPUT_FILE="$WDIR/cellbender_data/$TAG/raw_gene_bc_matrices_h5.h5"
+h5=raw_gene_bc_matrices_h5.h5
+outdir=cellbender
 
 ###################### DONT CHANGE OPTIONS BELOW THIS LINE ###########################
 
-singularity run --nv --bind /nfs,/lustre $CELLBENDER_IMAGE cellbender remove-background \
-    --input "$INPUT_FILE" \
-    --output "${OUTPUT_FOLDER}/cellbender_out.h5" \
+[[ ! -e "$h5" ]] && echo "No $h5 in $dir" && false
+
+echo $dir ok
+mkdir -p $outdir
+
+singularity run --nv --bind /nfs,/lustre $im cellbender remove-background \
+    --input $h5 \
+    --output "${outdir}/cellbender_out.h5" \
     --cuda \
-    --expected-cells $EXPECTED_CELLS \
-    --epochs $EPOCHS \
-    --total-droplets-included $DROPLETS \
-    --fpr $FPR \
-    --learning-rate $LEARN
+    --expected-cells $cells \
+    --epochs $epochs \
+    --total-droplets-included $droplets \
+    --fpr $fpr \
+    --learning-rate $learn
