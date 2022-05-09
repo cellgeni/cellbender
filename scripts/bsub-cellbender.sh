fb2339e8c5d8561dd539e8e57f7196a3ccf92763
Simon Murray <simondonaldmurray@gmail.com>
2022-05-09 11:16:24 +0100

    improved submission script

--- scripts/bsub-cellbender.sh
+++ scripts/bsub-cellbender.sh
@@ -5,20 +5,25 @@ set -euo pipefail
 #run this from work directory
 
 script=../actions/cellbender-0-2-0.sh #selecting cellbender script to run
-sf=../actions/samples.txt #selecting sample file
+sf=../actions/samples-10k-16k.txt #selecting sample file
 
 CPU=8 #selecting cpus
 MEM=40000 #selecting memory 
 GROUP="cellgeni" #selecting group to submit with 
 QUE="gpu-cellgeni" #selecting queue to submit to
-GPU_MODE="mode=shared:j_exclusive=no:gmem=6000:num=1" #selecting gpu conditions: memory, gpu cores, whether to shared gpu
-GPU_MODEL="dgx-b11" #selecting gpu model
+GMEM=6000 #selecting gpu conditions: memory, gpu cores, whether to shared gpu
+MODEL="" #selecting gpu model
+#MODEL="dgx-b11" #selecting gpu model
 
 ###################### DONT CHANGE OPTIONS BELOW THIS LINE ###########################
 
 if true; then
   mkdir -p logs
   cat $sf | while read name; do
-    bsub -n $CPU -Rspan[hosts=1] -M $MEM -R"select[mem>${MEM}] rusage[mem=${MEM}]" -G $GROUP -q $QUE -gpu $GPU_MODE -m $GPU_MODEL -o logs/ooo.$name.%J.txt -e logs/eee.$name.%J.txt $script $name
+    if [ "$MODEL" != "" ]; then
+      bsub -n $CPU -Rspan[hosts=1] -M $MEM -R"select[mem>${MEM}] rusage[mem=${MEM}]" -G $GROUP -q $QUE -gpu "mode=shared:j_exclusive=no:gmem=${GMEM}:num=1" -m $MODEL -o logs/ooo.$name.%J.txt -e logs/eee.$name.%J.txt $script $name
+    else
+      bsub -n $CPU -Rspan[hosts=1] -M $MEM -R"select[mem>${MEM}] rusage[mem=${MEM}]" -G $GROUP -q $QUE -gpu "mode=shared:j_exclusive=no:gmem=${GMEM}:num=1" -o logs/ooo.$name.%J.txt -e logs/eee.$name.%J.txt $script $name
+    fi
   done
 fi
