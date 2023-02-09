791c11fef60def4dcf6b8141d409d6fb891f399b
Prete <martin.prete@sanger.ac.uk>
2023-02-09 11:56:00 +0000

    Update bsub-cellbender.sh
    - Fix comment on queue for script submisssion. (GMEM <30 for gpu-normal/basment and GMEM>30 cellgeni-a100) 
    - The model selection was removed because A100s don't share queue with V100 anymore.
    - Removed if ture; then (why was that there?)

--- scripts/bsub-cellbender.sh
+++ scripts/bsub-cellbender.sh
@@ -11,21 +11,14 @@ sf=../actions/sample-list  #selecting sample file
 CPU=8 #selecting cpus
 MEM=40000 #selecting memory 
 GROUP="cellgeni" #selecting group to submit with 
-#QUE="gpu-cellgeni-a100" #selecting queue to submit to
-QUE="gpu-normal" #if GMEM > 30GB then can use gpu-normal or gpu-huge or gpu-basement
 GMEM=6000 #selecting gpu conditions: memory, gpu cores, whether to shared gpu
-MODEL="" #selecting gpu model
-#MODEL="dgx-b11" #selecting gpu model
+QUE="gpu-normal" #selecting queue to submit to
+# if GMEM < 30GB you can use "gpu-normal", "gpu-huge" or "gpu-basement"
+# if you need GMEM >30GB use QUE="gpu-cellgeni-a100"
 
 ###################### DONT CHANGE OPTIONS BELOW THIS LINE ###########################
 
-if true; then
-  mkdir -p logs
-  cat $sf | while read name; do
-    if [ "$MODEL" != "" ]; then
-      bsub -n $CPU -Rspan[hosts=1] -M $MEM -R"select[mem>${MEM}] rusage[mem=${MEM}]" -G $GROUP -q $QUE -gpu "mode=shared:j_exclusive=no:gmem=${GMEM}:num=1" -m $MODEL -o logs/ooo.$name.%J.txt -e logs/eee.$name.%J.txt $script $name
-    else
-      bsub -n $CPU -Rspan[hosts=1] -M $MEM -R"select[mem>${MEM}] rusage[mem=${MEM}]" -G $GROUP -q $QUE -gpu "mode=shared:j_exclusive=no:gmem=${GMEM}:num=1" -o logs/ooo.$name.%J.txt -e logs/eee.$name.%J.txt $script $name
-    fi
-  done
-fi
+mkdir -p logs
+cat $sf | while read name; do
+   bsub -n $CPU -Rspan[hosts=1] -M $MEM -R"select[mem>${MEM}] rusage[mem=${MEM}]" -G $GROUP -q $QUE -gpu "mode=shared:j_exclusive=no:gmem=${GMEM}:num=1" -o logs/ooo.$name.%J.txt -e logs/eee.$name.%J.txt $script $name
+done
