67dddfbbee8609665d278e5752a799b2c9c49732
Cellgeni Service <cellgeni-su@sanger.ac.uk>
2024-03-05 10:34:32 +0000

    clean script and update README

--- README.md
+++ README.md
@@ -51,14 +51,16 @@ then edit the `cellbender-0-2-0.sh` script.
 Jobs will be submitted to the FARM, they can be monitored with the command `bjobs` or you can look at the log and error files within the `logs` subdirectory.
 
 ### Quality Control
-
-Running the script `cellbender.qc.R` will assess the quality of the output. It runs on a single cellbender output directory at a time. I.e.
-
+Running the script `cellbender.qc.R` will assess the quality of the output. It runs on a single cellbender output directory at a time. 
+We do not have singularity image that can be used to run the scripr right now, so here is temporary solution:
 ```bash
-cd data/sampleID
-Rscript cellbender.qc.R cellbender.qc.R -m 3 -v -w . > qc.txt
+export R_LIBS_USER=/nfs/cellgeni/pasham/R/%p-library/%v
+cd work
+/software/cellgeni/wrappers/r-4.3.1/Rscript cellbender.qc.R -v -m 3 . 
 ```
 
-Please note this script installs some packages so there needs to be a writable path in r libs path.
+### Multiomes
+If running cellbender on multiome samples. Run the `bsub < bsub.extract.gex.from.multiome.sh` script and then use the `cellbender-matrix_multiome.sh` to run cellbender.
 
-If running cellbender on multiome samples. Run the `bsub < bsub.extract.gex.from.multiome.sh` script and then use the `cellbender-matrix_multiome.sh` to run cellbender. Cellbender produces h5 files that cannot be read by scanpy if it used mtx as input. So, run `bsub < bsub.cellbender_output_to_mtx.sh` to transform cellbender output into mtx.
+### If mtx input was used
+Cellbender produces h5 files that cannot be read by scanpy if it used mtx as input (STARsolo or multiomes). So, run `bsub < actions/cellbender/scripts/bsub.cellbender_output_to_mtx.sh` to transform cellbender output into mtx. It will find all cellbender outputs in working directory and adds mtx folder to them.
--- scripts/cellbender_output_to_mtx.R
+++ scripts/cellbender_output_to_mtx.R
@@ -1,41 +1,42 @@
 library(hdf5r)
 library(Matrix)
+# cellbender v2 outputs invalid h5 if it starts from mtx (no genome in header) so scanpy cannot read it
+# this script converts h5 into mtx format so anyone can read it
+# find all cellbender outputs in current folder recursively
 # convert to mtx #########
 
-sids = readLines('actions/sample-list')
-for(sid in sids){
-  if(!file.exists(paste0('work/',sid,'/cellbender_out.h5'))) next
-  infile = hdf5r::H5File$new(filename = paste0('work/',sid,'/cellbender_out.h5'), mode = "r")
+# function takes cellbender outdir as input and creates subfolder there with unfiltered counts in mtx format
+h5_to_mtx = function(cb_out,outdir=paste0(cb_out,'/cellbender_out')){
+  h5file = paste0(cb_out,'/cellbender_out.h5')
+  infile = hdf5r::H5File$new(filename = h5file, mode = "r")
   n=names(infile)
+  # counts
   cnts = sparseMatrix(i=infile[[n]][['indices']]$read()+1, p=infile[[n]][['indptr']]$read(),x = as.numeric(infile[[n]][['data']]$read()),dims=infile[[n]][['shape']]$read())
+  # barcodes
   barcodes = infile[[n]][['barcodes']]$read()
+  # features
   fnames = c('id','name','feature_type')
   features = as.data.frame(lapply(fnames,function(nn)infile[[n]][['features']][[nn]]$read()))
-  colnames(features) = fnames
-  infile$close_all()
   
-  dir.create(paste0('work/',sid,'/cellbender_out'),showWarnings = FALSE)
-  gz1 = gzfile(paste0('work/',sid,'/cellbender_out/features.tsv.gz'), "w")
+  # save; I'll not delete folder if exists, just overwrite
+  dir.create(outdir,showWarnings = FALSE)
+  
+  gz1 = gzfile(paste0(outdir,'/features.tsv.gz'), "w")
   write.table(features,gz1,quote = FALSE,sep='\t',row.names = FALSE,col.names = FALSE)
   close(gz1)
   
-  gz1 = gzfile(paste0('work/',sid,'/cellbender_out/barcodes.tsv.gz'), "w")
+  gz1 = gzfile(paste0(outdir,'/barcodes.tsv.gz'), "w")
   writeLines(barcodes,gz1)
   close(gz1)
   
-  if(file.exists(paste0('work/',sid,'/cellbender_out/matrix.mtx.gz'))) file.remove(paste0('work/',sid,'/cellbender_out/matrix.mtx.gz'))
-  if(file.exists(paste0('work/',sid,'/cellbender_out/matrix.mtx'))) file.remove(paste0('work/',sid,'/cellbender_out/matrix.mtx'))
-  writeMM(cnts,paste0('work/',sid,'/cellbender_out/matrix.mtx')) 
-  system({paste0("gzip 'work/",sid,"/cellbender_out/matrix.mtx'")})
-  
+  writeMM(cnts,paste0(outdir,'/matrix.mtx')) 
+  system({paste0("gzip -f '",outdir,"/matrix.mtx'")})
 }
 
-# QC ###############
-# qc = read.table('/warehouse/cellgeni/tic-2055/work/qc.txt',header = TRUE)
-# qc$cellsf = qc$common.detected.cells/(qc$probable.cells + qc$additional.cells)
-# qc[qc$cellsf>0.99,]
-# qc[order(qc$cellsf,decreasing = TRUE)[1:15],]
-# 
-# 
-# plot(qc$cellsf,qc$median.soup.fraq)
-# fisher.test(table(qc$cellsf>0.99,qc$median.soup.fraq<0.01))
+
+cb_outs = list.files('.',recursive = TRUE,pattern = 'cellbender_out.h5')
+cb_outs = sub('cellbender_out.h5$','',cb_outs)
+for(o in cb_outs){
+  print(o)
+  h5_to_mtx(o)
+}
